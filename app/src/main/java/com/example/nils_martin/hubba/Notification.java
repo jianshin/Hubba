b6ada3d66879d7b688f459cc8a0bf0f3b1f6754f
Johannes <Jogust95@gmail.com>
2018-09-23 22:41:35 +0200

    InitialCommitNotifications
    Added the Notification Class with the three empty void methods habitWraper(), habitNotification() and achievemntNotification

---
+++ app/src/main/java/com/example/nils_martin/hubba/Notification.java
@@ -0,0 +1,14 @@
+package com.example.nils_martin.hubba;
+
+public class Notification {
+
+    public void notificationWraper (Habit habit){
+
+    }
+    public void habitNotification (Habit habit){
+
+    }
+    public void achievementNotification (Achievement achievement){
+
+    }
+}
